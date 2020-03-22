ff304d60eb130bd7cd6f2cd8f638eafcc1a3982f
Golapadeog <jeremy.landon@outlook.com>
2020-03-22 13:43:18 +0100

    simplify code for demo

--- src/RequestResponse/Program.cs
+++ src/RequestResponse/Program.cs
@@ -1,37 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
 using Microsoft.Azure.ServiceBus.Management;
 
 namespace RequestResponse
 {
     class Program
     {
-        const string ConnectionString = "<your_connection_string>";
-        const string RequestQueueName = "sample.request";
-        const string ReplyQueueName = "sample.reply";
-
         static async Task Main()
         {
+            var connectionString = "<your_connection_string>";
+            var requestQueueName = "sample.request";
+            var replyQueueName = "sample.reply";
+
             // create queues
-            await Task.WhenAll(CreateQueueAsync(RequestQueueName, false), CreateQueueAsync(ReplyQueueName, true));
+            await Task.WhenAll(CreateQueueAsync(connectionString, requestQueueName, false), CreateQueueAsync(connectionString, replyQueueName, true));
 
             // init threads
             var threads = new List<Thread>();
             for (var i = 0; i < 3; i++)
             {
                 var threadId = "requestor-" + i;
-                threads.Add(new Thread(async () => await GetRequestorThread(threadId)));
+                threads.Add( SampleThreadFactory.CreateRequestor(threadId, connectionString, requestQueueName, replyQueueName));
             }
 
             for (var i = 0; i < 2; i++)
             {
                 var threadId = "replier-" + i;
-                threads.Add(new Thread(() => GetReplierThread(threadId)));
+                threads.Add(SampleThreadFactory.CreateReplier(threadId, connectionString, requestQueueName));
             }
 
             // start all
@@ -39,101 +36,10 @@ namespace RequestResponse
 
             Console.Read();
         }
-
-        private static async Task GetRequestorThread(string threadId)
+        
+        static async Task CreateQueueAsync(string connectionString, string queueName, bool requiresSession)
         {
-            var messageSender = new MessageSender(ConnectionString, RequestQueueName);
-            var sessionId = Guid.NewGuid().ToString();
-
-            var message = new Message
-            {
-                MessageId = threadId,
-                ReplyTo = new ServiceBusConnectionStringBuilder(ConnectionString) { EntityPath = ReplyQueueName }.ToString(),
-                ReplyToSessionId = sessionId,
-                TimeToLive = TimeSpan.FromMinutes(2)
-            };
-
-            LogClient($"{threadId} send a message to '{RequestQueueName}' with replyToSessionId='{message.ReplyToSessionId}' and entityPath='{ReplyQueueName}'", "send", message);
-            await messageSender.SendAsync(message);
-
-            SessionClient sessionClient = new SessionClient(ConnectionString, ReplyQueueName);
-            var session = await sessionClient.AcceptMessageSessionAsync(sessionId);
-
-            LogClient($"{threadId}'s waiting a reply message from '{ReplyQueueName}' with sessionId='{sessionId}'...", "wait", null);
-            Message sessionMessage = await session.ReceiveAsync();
-
-            LogClient($"{threadId} received a reply message from '{ReplyQueueName}' with sessionId='{sessionMessage.SessionId}'", "receive", sessionMessage);
-        }
-
-        private static void GetReplierThread(string threadId)
-        {
-            var messageReceiver = new MessageReceiver(ConnectionString, RequestQueueName);
-
-            messageReceiver.RegisterMessageHandler(
-                async (message, cancellationToken) =>
-                {
-                    LogWorker($"{threadId} received a reply message from '{RequestQueueName}' with replyToSessionId='{message.ReplyToSessionId}'", "receive", message);
-                    var connectionStringBuilder = new ServiceBusConnectionStringBuilder(message.ReplyTo);
-                    var replyToQueue = new MessageSender(connectionStringBuilder);
-                    var replyMessage = new Message(Encoding.UTF8.GetBytes($"processed by {threadId}"))
-                    {
-                        CorrelationId = message.MessageId,
-                        SessionId = message.ReplyToSessionId,
-                        TimeToLive = TimeSpan.FromMinutes(2)
-                    };
-
-                    /****  Simulate an action   *****/
-                    await Task.Delay(new Random().Next(1000, 2000), cancellationToken);
-                    /*******************************/
-
-                    LogWorker($"{threadId} send a reply message to '{connectionStringBuilder.EntityPath}' with sessionId='{message.ReplyToSessionId}'", "send", replyMessage);
-                    await replyToQueue.SendAsync(replyMessage);
-                },
-                new MessageHandlerOptions(args => throw args.Exception)
-                {
-                    MaxConcurrentCalls = 1
-                });
-        }
-
-
-        #region helper 
-
-        private static void LogClient(string logMessage, string action, Message message)
-        {
-            Log(logMessage, action, message, ConsoleColor.Cyan);
-        }
-
-        private static void LogWorker(string logMessage, string action, Message message)
-        {
-            Log(logMessage, action, message, ConsoleColor.Red);
-        }
-
-        private static void Log(string logMessage, string action, Message message, ConsoleColor color)
-        {
-            lock (Console.Out)
-            {
-                Console.ForegroundColor = color;
-                Console.WriteLine($"- {logMessage} [{DateTime.Now:hh:mm:ss.fff tt}]");
-                Console.ResetColor();
-                if (action != null)
-                    Console.WriteLine($"\t action={action}");
-                if (message?.Body != null)
-                    Console.WriteLine($"\t Body={ Encoding.UTF8.GetString(message.Body) }");
-                if (message?.MessageId != null)
-                    Console.WriteLine($"\t MessageId={ message.MessageId}");
-                if (message?.CorrelationId != null)
-                    Console.WriteLine($"\t CorrelationId={ message.CorrelationId}");
-                if (message?.SessionId != null)
-                    Console.WriteLine($"\t SessionId={ message.SessionId}");
-                if (message?.ReplyToSessionId != null)
-                    Console.WriteLine($"\t ReplyToSessionId={ message.ReplyToSessionId}");
-                Console.WriteLine();
-            }
-        }
-
-        static async Task CreateQueueAsync(string queueName, bool requiresSession)
-        {
-            var managementClient = new ManagementClient(ConnectionString);
+            var managementClient = new ManagementClient(connectionString);
 
             if (await managementClient.QueueExistsAsync(queueName))
             {
@@ -145,6 +51,5 @@ namespace RequestResponse
                 RequiresSession = requiresSession
             });
         }
-        #endregion
     }
 }
---
+++ src/RequestResponse/SampleThreadFactory.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+
+namespace RequestResponse
+{
+    public static class SampleThreadFactory
+    {
+        public static Thread CreateRequestor(string threadId, string connectionString, string requestQueueName, string replyQueueName)
+        {
+            return new Thread(async () =>
+            {
+                var messageSender = new MessageSender(connectionString, requestQueueName);
+                var sessionId = Guid.NewGuid().ToString();
+
+                var message = new Message
+                {
+                    MessageId = threadId,
+                    ReplyTo = new ServiceBusConnectionStringBuilder(connectionString) { EntityPath = replyQueueName }.ToString(),
+                    ReplyToSessionId = sessionId,
+                    TimeToLive = TimeSpan.FromMinutes(2)
+                };
+
+                LogClient($"{threadId} send a message to '{requestQueueName}' with replyToSessionId='{message.ReplyToSessionId}' and entityPath='{replyQueueName}'", "send", message);
+                await messageSender.SendAsync(message);
+
+                SessionClient sessionClient = new SessionClient(connectionString, replyQueueName);
+                var session = await sessionClient.AcceptMessageSessionAsync(sessionId);
+
+                LogClient($"{threadId}'s waiting a reply message from '{replyQueueName}' with sessionId='{sessionId}'...", "wait", null);
+                Message sessionMessage = await session.ReceiveAsync();
+
+                LogClient($"{threadId} received a reply message from '{replyQueueName}' with sessionId='{sessionMessage.SessionId}'", "receive", sessionMessage);
+            });
+        }
+
+        public static Thread CreateReplier(string threadId, string connectionString, string requestQueueName)
+        {
+            return new Thread(() =>
+            {
+                var messageReceiver = new MessageReceiver(connectionString, requestQueueName);
+
+                messageReceiver.RegisterMessageHandler(
+                     async (message, cancellationToken) =>
+                     {
+                         LogWorker($"{threadId} received a reply message from '{requestQueueName}' with replyToSessionId='{message.ReplyToSessionId}'", "receive", message);
+                         var connectionStringBuilder = new ServiceBusConnectionStringBuilder(message.ReplyTo);
+                         var replyToQueue = new MessageSender(connectionStringBuilder);
+                         var replyMessage = new Message(Encoding.UTF8.GetBytes($"processed by {threadId}"))
+                         {
+                             CorrelationId = message.MessageId,
+                             SessionId = message.ReplyToSessionId,
+                             TimeToLive = TimeSpan.FromMinutes(2)
+                         };
+
+                         /****  Simulate an action  *****/
+                         await Task.Delay(new Random().Next(1000, 2000), cancellationToken);
+                         /*******************************/
+
+                         LogWorker($"{threadId} send a reply message to '{connectionStringBuilder.EntityPath}' with sessionId='{message.ReplyToSessionId}'", "send", replyMessage);
+                         await replyToQueue.SendAsync(replyMessage);
+                     },
+                     new MessageHandlerOptions(args => throw args.Exception)
+                     {
+                         MaxConcurrentCalls = 1
+                     });
+            });
+        }
+
+        #region log
+        private static void LogClient(string logMessage, string action, Message message)
+        {
+            Log(logMessage, action, message, ConsoleColor.Cyan);
+        }
+
+        private static void LogWorker(string logMessage, string action, Message message)
+        {
+            Log(logMessage, action, message, ConsoleColor.Red);
+        }
+
+        private static void Log(string logMessage, string action, Message message, ConsoleColor color)
+        {
+            lock (Console.Out)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine($"- {logMessage} [{DateTime.Now:hh:mm:ss.fff tt}]");
+                Console.ResetColor();
+                if (action != null)
+                    Console.WriteLine($"\t action={action}");
+                if (message?.Body != null)
+                    Console.WriteLine($"\t Body={ Encoding.UTF8.GetString(message.Body) }");
+                if (message?.MessageId != null)
+                    Console.WriteLine($"\t MessageId={ message.MessageId}");
+                if (message?.CorrelationId != null)
+                    Console.WriteLine($"\t CorrelationId={ message.CorrelationId}");
+                if (message?.SessionId != null)
+                    Console.WriteLine($"\t SessionId={ message.SessionId}");
+                if (message?.ReplyToSessionId != null)
+                    Console.WriteLine($"\t ReplyToSessionId={ message.ReplyToSessionId}");
+                Console.WriteLine();
+            }
+        }
+        #endregion
+    }
+}
